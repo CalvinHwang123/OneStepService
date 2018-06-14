a1ed043f5a0f203fc9ae3d89f39304536968096b
CalvinHwang123 <CalvinHwang123@gmail.com>
2018-06-14 09:47:03 +0800

    江海鑫 2018-6-13 9点

--- src/oss/biz/SystemManegeBizImpl.java
+++ src/oss/biz/SystemManegeBizImpl.java
@@ -1,16 +1,27 @@
 package oss.biz;
 
+import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import oss.bean.Powers;
+import oss.mapper.SystemManegeMapper;
+
 /*
  * 系统管理Biz业务实现类
  */
 @Service
-public class SystemManegeBizImpl implements SystemManegeBiz{
+public class SystemManegeBizImpl implements SystemManegeBiz {
 	@Resource
-	private SystemManegeBiz systemManegeBizImpl;
+	private SystemManegeMapper systemManegeMapper;
+
+	// jhx 获取权限列表 2018-6-13 9:00
+	@Override
+	public List<Powers> powerList() {
+		// TODO Auto-generated method stub
+		return systemManegeMapper.listPower();
+	}
 
 }
--- src/oss/mapper/SystemManegeMapper.java
+++ src/oss/mapper/SystemManegeMapper.java
@@ -1,10 +1,15 @@
 package oss.mapper;
 
+import java.util.List;
 
 import oss.annotation.MyRepository;
+import oss.bean.Powers;
+
 /*
- * 雇主Mapper接口
+ * 系统管理Mapper接口
  */
 @MyRepository
 public interface SystemManegeMapper {
+	// jhx 获取权限列表 2018-6-13 9:00
+	public List<Powers> listPower();
 }
